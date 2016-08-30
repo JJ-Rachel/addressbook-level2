ad64d117a3b7689aeaa8086f75ef902ee130c948
Rachel Yang <frozensausage@gmail.com>
2016-08-30 20:54:00 +0800

    Add Unit class

---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.data.person;
+
+public class Unit {
+	private String unit;
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+}
