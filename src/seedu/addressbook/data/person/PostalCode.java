023d6e87be0410eac4978dd32ebe6ea32ed59e61
Rachel Yang <frozensausage@gmail.com>
2016-08-30 20:56:01 +0800

    Add PostalCode class

---
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.data.person;
+
+public class PostalCode {
+	private String postalCode;
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+}
