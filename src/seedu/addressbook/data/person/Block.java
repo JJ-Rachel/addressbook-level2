d822aab79eca7e1407f2f472cc6ee1a3d62760ed
Rachel Yang <frozensausage@gmail.com>
2016-08-30 20:48:11 +0800

    Add Block class

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+public class Block {
+	private String block;
+
+	public String getBlock() {
+		return block;
+	}
+
+	public void setBlock(String block) {
+		this.block = block;
+	}
+	
+	
+}
