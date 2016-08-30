f38bd08c662829b9441ec2fb483ee593dd1b2b1d
Rachel Yang <frozensausage@gmail.com>
2016-08-30 20:52:27 +0800

    Add Street class

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.data.person;
+
+public class Street {
+	private String street;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+}
