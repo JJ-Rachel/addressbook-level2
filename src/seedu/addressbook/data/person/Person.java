475faf30f48ca50f606e8b8ab754fa2b5d167f37
Rachel Yang <frozensausage@gmail.com>
2016-09-07 00:38:53 +0800

    Add printable interface and let Address, Email, Name, Phone classes implement it, and add printable varargs method to Person class

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated.
  */
-public class Person implements ReadOnlyPerson {
+public class Person implements ReadOnlyPerson{
 
     private Name name;
     private Phone phone;
@@ -83,5 +83,12 @@ public class Person implements ReadOnlyPerson {
     public String toString() {
         return getAsTextShowAll();
     }
-
+    
+    public String getPrintableString(Printable... printables){
+    	String printableString  = ""; 
+    	for (Printable printable: printables) {
+    		printableString = printableString + printable.getPrintableString() + " ";
+    	}
+		return printableString;
+    }
 }
