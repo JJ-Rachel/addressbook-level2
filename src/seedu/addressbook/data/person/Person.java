2a5f99dcf92c5fb34ffffeee5efb99c510ff09e1
Rachel Yang <frozensausage@gmail.com>
2016-09-06 22:49:08 +0800

    Add class variable nextSequenceNumber and instance variable sequenceNumber and accessor method getSequenceNumber()

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -16,6 +16,9 @@ public class Person implements ReadOnlyPerson {
     private Address address;
 
     private final UniqueTagList tags;
+    
+    private static int nextSequenceNumber = 1;
+    private int sequenceNumber;
     /**
      * Assumption: Every field must be present and not null.
      */
@@ -25,6 +28,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.sequenceNumber = nextSequenceNumber;
+        nextSequenceNumber++;
     }
 
     /**
@@ -57,6 +62,11 @@ public class Person implements ReadOnlyPerson {
     @Override
     public UniqueTagList getTags() {
         return new UniqueTagList(tags);
+    }
+    
+    @Override
+    public int getSequenceNumber() {
+    	return sequenceNumber;
     }
 
     /**
